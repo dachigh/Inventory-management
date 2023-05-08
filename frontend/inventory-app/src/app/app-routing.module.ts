7f6d86ce9ec79d12c2bdf8a8ec3630140ea7a6f1
dachigh <d_ghoniadze@cu.edu.ge>
2023-05-08 20:43:09 +0400

    changed routes to match endpoints

--- frontend/inventory-app/src/app/app-routing.module.ts
+++ frontend/inventory-app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AddComponent } from './add/add.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddComponent } from './components/add/add.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/inventories', pathMatch: 'full' },
+  { path: 'inventories', component: HomeComponent },
   { path: 'add', component: AddComponent }
 ];
 
