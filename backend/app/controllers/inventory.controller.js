4b4f603cb17234c73bc4a82bbc189b48fc18d8b4
dachigh <d_ghoniadze@cu.edu.ge>
2023-05-08 23:13:41 +0400

    Created server side pagination

--- backend/app/controllers/inventory.controller.js
+++ backend/app/controllers/inventory.controller.js
@@ -2,6 +2,21 @@ const db = require("../models/connectToDB");
 const Inventory = db.inventories;
 const Op = db.Sequelize.Op;
 
+const getPagination = (page, size) => {
+  const limit = size ? +size : 3;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+  const { count: totalItems, rows: inventories } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, inventories, totalPages, currentPage };
+};
+
 // Create and Save a new Inventory
 exports.create = (req, res) => {
   // Validate request
@@ -36,34 +51,54 @@ exports.create = (req, res) => {
 
 // Retrieve all Inventories from the database.
 exports.findAll = (req, res) => {
-  const place = req.query.place;
-  var condition = place ? { place: { [Op.eq]: place } } : null;
+  const { page, size, place } = req.query;
+  var condition = place ? { place: { [Op.eq]: `${place}` } } : null;
 
-  const sortBy = req.query.sortBy;
-  var order = [['title', 'ASC']]; // Default sort order
+  const { limit, offset } = getPagination(page, size);
 
-  if (sortBy === 'titleAsc') {
-    order = [['title', 'ASC']];
-  } else if (sortBy === 'titleDesc') {
-    order = [['title', 'DESC']];
-  } else if (sortBy === 'priceAsc') {
-    order = [['price', 'ASC']];
-  } else if (sortBy === 'priceDesc') {
-    order = [['price', 'DESC']];
-  }
-
-
-  Inventory.findAll({ where: condition, order: order })
+  Inventory.findAndCountAll({ where: condition, limit, offset })
     .then(data => {
-      res.send(data);
+      const response = getPagingData(data, page, limit);
+      res.send(response);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving inventories."
+          err.message || "Some error occurred while retrieving tutorials."
       });
     });
 };
+
+
+// exports.findAll = (req, res) => {
+//   const place = req.query.place;
+//   var condition = place ? { place: { [Op.eq]: place } } : null;
+
+//   const sortBy = req.query.sortBy;
+//   var order = [['title', 'ASC']]; // Default sort order
+
+//   if (sortBy === 'titleAsc') {
+//     order = [['title', 'ASC']];
+//   } else if (sortBy === 'titleDesc') {
+//     order = [['title', 'DESC']];
+//   } else if (sortBy === 'priceAsc') {
+//     order = [['price', 'ASC']];
+//   } else if (sortBy === 'priceDesc') {
+//     order = [['price', 'DESC']];
+//   }
+
+
+//   Inventory.findAll({ where: condition, order: order })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving inventories."
+//       });
+//     });
+// };
 
 
 // Delete a Inventory with the specified id in the request
